3533f1d142448e40a1d5479de6579f86682d9ac3
nikhil <anikhil03prasanna@gmail.com>
2022-11-29 18:57:40 +0530

    File Changes

--- digital-business-card/src/Header.js
+++ digital-business-card/src/Header.js
@@ -3,12 +3,14 @@ import React from "react";
 export default function Header() {
     return (
         <div>
-            <img src="1651134580467.jpg" alt='' width="96%" height="350px" className='image'></img>
+            <img src="IMG20220322175840.jpg" alt='' width="96%" className='image'></img>
             <h2 className='name'>Nikhil Prasanna</h2>
             <h4 className='profession'>Backend Developer</h4>
 
             <button className='button-1'>✉️Email</button>
             <button className='button-2'>📞Mobile</button>
+            <br></br>
+            <br></br>
         </div>
     )
 }
