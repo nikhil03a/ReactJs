4bd36cef6d78e5157fa2534426ede12c32cf8ba1
nikhil <anikhil03prasanna@gmail.com>
2022-11-29 20:54:07 +0530

    File Changes

--- digital-business-card/src/Header.js
+++ digital-business-card/src/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
         <div>
             <img src="IMG20220322175840.jpg" alt='' width="96%" className='image'></img>
             <h2 className='name'>Nikhil Prasanna</h2>
-            <h4 className='profession'>Backend Developer</h4>
+            <h4 className='profession'>Frontend Developer</h4>
 
             <button className='button-1'>✉️Email</button>
             <button className='button-2'>📞Mobile</button>
