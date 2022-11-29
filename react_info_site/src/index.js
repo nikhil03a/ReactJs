be28844e0ea81070832c0ad88e8be033e414ab24
nikhil <anikhil03prasanna@gmail.com>
2022-11-29 15:10:53 +0530

    File Changes

--- react_info_site/src/index.js
+++ react_info_site/src/index.js
@@ -5,21 +5,52 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+function Header() {
+  return (
+    <header>
+      <nav className='nav'>
+        <img src="logo192.png" alt="" className='logo'></img>
+        <ul className="nav-items">
+          <li>About</li>
+          <li>Pricing</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+function Footer() {
+  return (
+    <footer className='footer'>
+      ©️ 2022 Web Development Team. All rights reserved.
+    </footer>
+  )
+}
+function MainContent() {
+  return (
+    <div className='content'>
+      <h1>Fun Facts About React</h1>
+      <ol>
+        <li>Was First Released in 2013</li>
+        <li>Was Originally created by Jordan Walker</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ol>
+    </div>
+  )
+}
+function Temporary() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
 
-const element= (
-  <div>
-    <img src="logo192.png" alt="" width="100px" height="100px"></img>
-    <h1>Fun Facts About React</h1>
-    <ol>
-      <li>Was First Released in 2013</li>
-      <li>Was Originally created by Jordan Walker</li>
-      <li>Has well over 100k stars on GitHub</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ol>
-  </div>
-)
-root.render(element);
+root.render(<Temporary />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
