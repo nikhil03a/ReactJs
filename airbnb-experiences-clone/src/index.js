6084b5e8d2e28964fd0acea4f577a3317ffb56c1
nikhil <anikhil03prasanna@gmail.com>
2022-11-30 15:13:34 +0530

    File Changes

--- airbnb-experiences-clone/src/index.js
+++ airbnb-experiences-clone/src/index.js
@@ -5,11 +5,26 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Bnb() {
+  return (
+    <div>
+      <nav>
+        <ul className='navlist'>
+          <li className='nav-items'><img src="logo.webp" width="200px" height="80px"></img></li>
+          <li className='nav-items'><h2>airbnb</h2></li>
+        </ul>
+      </nav>
+      <div className='top-content'>
+        <img src="grid.png" className='grid-image'></img>
+        <h2 className='heading-1'>Online Experiences</h2>
+        <p className='heading-2'>Join unique interactive activities led by one-of-a-kind hosts all without 
+          leaving home
+        </p>
+      </div>
+    </div>
+  )
+}
+root.render(<Bnb />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
