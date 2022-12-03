ccbe32e45fe81ee4338aafca0f0afb3e4ee6796d
nikhil <anikhil03prasanna@gmail.com>
2022-12-03 21:32:30 +0530

    File changes

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import MemesData from './MemesData';
+import React from 'react';
 
 function App() {
+  const [meme, setMeme] = React.useState(
+    {
+      topText: "",
+      bottomText: "",
+      url: ""
+    }
+  );
+  const [allMemeImages, setAllMemeImages] = React.useState(MemesData);
+  function getRandomMeme() {
+    const memeArray = MemesData.data.memes;
+    const random = Math.floor(Math.random() * memeArray.length);
+    const newUrl = memeArray[random].url;
+    setMeme(previousContent => ({
+      ...previousContent,
+      url: newUrl
+    }))
+  }
   return (
     <div className="App">
       <div className='header'>
@@ -9,11 +28,12 @@ function App() {
         <h2>Meme Generator</h2>
       </div>
       <div className='main'>
-        <form >
+        <div >
           <input type='text' placeholder='---Top Text---'></input>
           <input type='text' placeholder='---Bottom Text---'></input><br></br>
-          <button >Get a new meme image</button>
-        </form>
+          <button onClick={getRandomMeme} >Get a new meme image</button>
+        </div>
+        <img src={meme.url}></img>
       </div>
     </div>
   );
