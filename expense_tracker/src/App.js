a5a611fe67b0f2863c60f39396eb32fd93949f44
nikhil03a <anikhil03prasanna@gmail.com>
2022-12-22 16:16:31 +0530

    File Changes

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
+      <div className='container'>
+        <Balance />
+      </div>
     </div>
   );
 }
