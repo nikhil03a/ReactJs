422da1eee8a44b2c41cabe5688f9eaf6f693f3de
nikhil03a <anikhil03prasanna@gmail.com>
2022-12-30 14:49:33 +0530

    File Changes

--- counter-app/src/GlobalState.js
+++ counter-app/src/GlobalState.js
@@ -13,7 +13,7 @@ export const GlobalProvider = ({children})=>{
     function setVal(num){
         setNumber(num);
     }
-    return (<GlobalContext.Provider value={{number,increase,decrease,setVal}}>
+    return (<GlobalContext.Provider value={{number,increase,decrease,setVal,setNumber}}>
         {children}
     </GlobalContext.Provider>)
 }
--- counter-app/src/SetValue.js
+++ counter-app/src/SetValue.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { GlobalContext } from './GlobalState'
 const SetValue = () => {
+    const {setVal} = React.useContext(GlobalContext);
+    const [num,setNum] = React.useState();
+    function handleChange(event){
+        setNum(event.target.value);
+    }
+    function handleClick(event){
+        setVal(num);
+        event.preventDefault();
+    }
   return (
     <div>
-      
+      <form>
+        <input type="number" onChange={handleChange} value={num}></input> 
+        <button onClick={handleClick}>SetValue</button>
+      </form>
     </div>
   )
 }
