390abf19c15ec225914dd1987a921b542b30f127
nikhil03a <anikhil03prasanna@gmail.com>
2023-03-05 14:51:28 +0530

    File Changes

--- context/src/App.js
+++ context/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <h1>{isOnline}</h1>
-      <button disabled={!isOnline}>{isOnline !? "Save" : "Reconnecting...."}</button>
+      <button disabled={!isOnline}>{isOnline ? "Save" : "Reconnecting...."}</button>
       <input type="text" value={firstName.value} onChange={firstName.onChange}></input>
       <h2>{firstName.value}</h2>
     </div>
